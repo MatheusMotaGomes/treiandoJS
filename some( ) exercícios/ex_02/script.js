b5812df79ef5a13f6704a4f8e8cd05ce7c1d16aa
Matheus Mota <matheusmgf2010@gmail.com>
2025-07-22 17:50:29 -0300

    Exercício 2

---
+++ some( ) exercícios/ex_02/script.js
@@ -0,0 +1,10 @@
+/*
+👉 Verifique se existe alguma fruta chamada "morango".
+Dica: use igualdade dentro do some().
+*/
+
+let frutas = ['maçã', 'banana', 'uva', 'laranja'];
+
+let existeMorango = frutas.some(fruta => fruta === 'morango');
+
+console.log(existeMorango);
