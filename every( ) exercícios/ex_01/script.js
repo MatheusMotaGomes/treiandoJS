42cadccfc8b9e0cbe9f24035e8976d92bd78c695
Matheus Mota <matheusmgf2010@gmail.com>
2025-07-23 11:51:25 -0300

    exercício 1

---
+++ every( ) exercícios/ex_01/script.js
@@ -0,0 +1,8 @@
+// Verifique se TODOS os números do array são positivos
+let numeros = [5, 10, 15, 20];
+
+// Use every() para verificar e exiba o resultado no console
+
+let positivos = numeros.every((numero) => numero >= 0);
+
+console.log(positivos);
